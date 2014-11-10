69232a9c1da4cf61873643c7081b20c319a8c784
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2014-11-10 22:00:02 +0100

    Error management

---
+++ src/error.c
@@ -0,0 +1,73 @@
+/*
+ * error.c --
+ *
+ * Implementation of error management in OptimPack library.
+ *
+ *-----------------------------------------------------------------------------
+ *
+ * Copyright (c) 2014 Éric Thiébaut
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ *-----------------------------------------------------------------------------
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "optimpack.h"
+
+static void
+default_error_handler(const char* message)
+{
+  fprintf(stderr, "ERROR: %s\n", message);
+  abort();
+}
+
+static void (*error_handler)(const char* message) = default_error_handler;
+
+opk_error_handler*
+opk_get_error_handler(void)
+{
+  return error_handler;
+}
+
+opk_error_handler*
+opk_set_error_handler(opk_error_handler* new_handler)
+{
+  opk_error_handler* old_handler = error_handler;
+  error_handler = (new_handler == NULL ? default_error_handler : new_handler);
+  return old_handler;
+}
+
+void
+opk_error(const char* reason)
+{
+  error_handler(reason);
+}
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * c-basic-offset: 2
+ * fill-column: 79
+ * coding: utf-8
+ * End:
+ */
