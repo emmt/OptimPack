c03ef82183bfb946cda25b40aee5cdb30bd22de2
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2015-05-02 17:05:45 +0200

    Add COBYLA, NEWUOA and BOBYQA algorithms

---
+++ src/cobyla.h
@@ -0,0 +1,228 @@
+/*
+ * cobyla.h -
+ *
+ * Definitions for Mike Powell's COBYLA algorithm for minimizing a function of
+ * a few variables.  The method is "derivatives free" (only the function values
+ * are needed) and accounts for constraints on the variables.  The algorithm is
+ * described in:
+ *
+ *   M.J.D. Powell, "A direct search optimization method that models the
+ *   objective and constraint functions by linear interpolation," in Advances
+ *   in Optimization and Numerical Analysis Mathematics and Its Applications,
+ *   vol. 275 (eds. Susana Gomez and Jean-Pierre Hennart), Kluwer Academic
+ *   Publishers, pp. 51-67 (1994).
+ *
+ * The present code is based on the original FORTRAN version written by Mike
+ * Powell who kindly provides his code on demand and has been converted to C by
+ * É. Thiébaut.
+ *
+ * Copyright (c) 1992, Mike Powell (FORTRAN version).
+ * Copyright (c) 2015, Éric Thiébaut (C version).
+ */
+
+#ifndef _COBYLA_H
+#define  _COBYLA_H 1
+
+#include "optimpack.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Prototype of the objective function assumed by the COBYLA routine.  The
+   returned value is the function value at X, N is the number of variables, M
+   is the number of constraints, X are the current values of the variables and
+   CON is to store the M constraints.  DATA is anything needed by the function
+   (unused by COBYLA itself). */
+typedef double cobyla_calcfc(opk_index_t n, opk_index_t m, const double x[],
+                           double con[], void* data);
+
+/*
+ * This subroutine minimizes an objective function F(X) subject to M inequality
+ * constraints on X, where X is a vector of variables that has N components.
+ * The algorithm employs linear approximations to the objective and constraint
+ * functions, the approximations being formed by linear interpolation at N+1
+ * points in the space of the variables.  We regard these interpolation points
+ * as vertices of a simplex.  The parameter RHO controls the size of the
+ * simplex and it is reduced automatically from RHOBEG to RHOEND.  For each RHO
+ * the subroutine tries to achieve a good vector of variables for the current
+ * size, and then RHO is reduced until the value RHOEND is reached.  Therefore
+ * RHOBEG and RHOEND should be set to reasonable initial changes to and the
+ * required accuracy in the variables respectively, but this accuracy should be
+ * viewed as a subject for experimentation because it is not guaranteed.  The
+ * subroutine has an advantage over many of its competitors, however, which is
+ * that it treats each constraint individually when calculating a change to the
+ * variables, instead of lumping the constraints together into a single penalty
+ * function.  The name of the subroutine is derived from the phrase Constrained
+ * Optimization BY Linear Approximations.
+ *
+ * The user must set the values of N, M, RHOBEG and RHOEND, and must provide an
+ * initial vector of variables in X.  Further, the value of IPRINT should be
+ * set to 0, 1, 2 or 3, which controls the amount of printing during the
+ * calculation.  Specifically, there is no output if IPRINT=0 and there is
+ * output only at the end of the calculation if IPRINT=1.  Otherwise each new
+ * value of RHO and SIGMA is printed.  Further, the vector of variables and
+ * some function information are given either when RHO is reduced or when each
+ * new value of F(X) is computed in the cases IPRINT=2 or IPRINT=3
+ * respectively.  Here SIGMA is a penalty parameter, it being assumed that a
+ * change to X is an improvement if it reduces the merit function:
+ *
+ *            F(X)+SIGMA*MAX(0.0,-C1(X),-C2(X),...,-CM(X)),
+ *
+ * where C1,C2,...,CM denote the constraint functions that should become
+ * nonnegative eventually, at least to the precision of RHOEND.  In the
+ * printed output the displayed term that is multiplied by SIGMA is
+ * called MAXCV, which stands for 'MAXimum Constraint Violation'.  The
+ * argument MAXFUN is an integer variable that must be set by the user to a
+ * limit on the number of calls of CALCFC, the purpose of this routine being
+ * given below.  The value of MAXFUN will be altered to the number of calls
+ * of CALCFC that are made.  The arguments W and IACT provide real and
+ * integer arrays that are used as working space.  Their lengths must be at
+ * least N*(3*N+2*M+11)+4*M+6 and M+1 respectively.
+ *
+ * In order to define the objective and constraint functions, we require a
+ * subroutine that has the name and arguments
+ *
+ *            SUBROUTINE CALCFC (N,M,X,F,CON)
+ *            DIMENSION X(*),CON(*)  .
+ *
+ * The values of N and M are fixed and have been defined already, while X is
+ * now the current vector of variables.  The subroutine should return the
+ * objective and constraint functions at X in F and CON(1),CON(2), ...,CON(M).
+ * Note that we are trying to adjust X so that F(X) is as small as possible
+ * subject to the constraint functions being nonnegative.
+ */
+extern int cobyla(opk_index_t n, opk_index_t m,
+                  cobyla_calcfc* calcfc, void* calcfc_data,
+                  double x[], double rhobeg, double rhoend,
+                  opk_index_t iprint, opk_index_t* maxfun, double w[], opk_index_t iact[]);
+
+/*
+ * Subroutine variant designed to be callable from FORTRAN code.  Usage is
+ * similar to that of `cobyla` except that everything is passed by address and
+ * that, in order to define the objective and constraint functions, we require
+ * a subroutine that has the name and arguments:
+ *
+ *            SUBROUTINE CALCFC (N,M,X,F,CON)
+ *            DIMENSION X(*),CON(*)
+ *
+ * The values of N and M are fixed and have been defined already, while X is
+ * now the current vector of variables.  The subroutine should return the
+ * objective and constraint functions at X in F and CON(1),CON(2), ...,CON(M).
+ * Note that we are trying to adjust X so that F(X) is as small as possible
+ * subject to the constraint functions being nonnegative.
+ */
+extern int cobyla_(opk_index_t* n, opk_index_t* m, double x[],
+                   double* rhobeg, double* rhoend, opk_index_t* iprint,
+                   opk_index_t* maxfun, double w[], opk_index_t iact[]);
+
+/* Possible values returned by COBYLA. */
+#define COBYLA_INITIAL_ITERATE         (2) /* only used internally */
+#define COBYLA_ITERATE                 (1) /* user requested to compute
+                                              F(X) and C(X) */
+#define COBYLA_SUCCESS                 (0) /* algorithm converged */
+#define COBYLA_ROUNDING_ERRORS        (-1)
+#define COBYLA_TOO_MANY_EVALUATIONS   (-2)
+#define COBYLA_BAD_ADDRESS            (-3)
+#define COBYLA_CORRUPTED              (-4)
+
+/* Opaque structure used by the reverse communication variant of COBYLA. */
+typedef struct _cobyla_context cobyla_context_t;
+
+/* Allocate a new reverse communication workspace for COBYLA algorithm.  The
+   returned address is `NULL` to indicate an error: either invalid parameters
+   (external variable `errno` set to `EINVAL`), or insufficient memory
+   (external variable `errno` set to `ENOMEM`).  When no longer needed, the
+   workspace must be deleted with `cobyla_delete`.
+
+   A typical usage is:
+   ```
+   double x[N], c[M], f;
+   cobyla_context_t* ctx;
+   x[...] = ...; // initial solution
+   ctx = cobyla_create(N, M, RHOBEG, RHOEND, IPRINT, MAXFUN);
+   status = cobyla_get_status(ctx);
+   while (status == COBYLA_ITERATE) {
+     f = ...; // compute function value at X
+     c[...] = ...; // compute constraints at X
+     status = cobyla_iterate(ctx, f, x, c);
+   }
+   cobyla_delete(ctx);
+   if (status != COBYLA_SUCCESS) {
+     fprintf(stderr, "Something wrong occured in COBYLA: %s\n",
+             cobyla_reason(status));
+   }
+   ```
+ */
+extern cobyla_context_t*
+cobyla_create(opk_index_t n, opk_index_t m, double rhobeg, double rhoend,
+              opk_index_t iprint, opk_index_t maxfun);
+
+/* Release ressource allocated for COBYLA reverse communication workspace.
+   Argument can be `NULL`. */
+extern void
+cobyla_delete(cobyla_context_t* ctx);
+
+/* Perform the next iteration of the reverse communication variant of the
+   COBYLA algorithm.  On entry, the wokspace status must be `COBYLA_ITERATE`,
+   `f` and `c` are the function value and the constraints at `x`.  On exit, the
+   returned value (the new wokspace status) is: `COBYLA_ITERATE` if a new trial
+   point has been stored in `x` and if user is requested to compute the
+   function value and the constraints on the new point; `COBYLA_SUCCESS` if
+   algorithm has converged and `x` has been set with the variables at the
+   solution (the corresponding function value can be retrieved with
+   `cobyla_get_last_f`); anything else indicate an error (see `cobyla_reason`
+   for an explanatory message). */
+extern int
+cobyla_iterate(cobyla_context_t* ctx, double f, double x[], double c[]);
+
+/* Restart COBYLA algorithm using the same parameters.  The return value is
+   the new status of the algorithm, see `cobyla_get_status` for details. */
+extern int
+cobyla_restart(cobyla_context_t* ctx);
+
+/* Get the current status of the algorithm.  Result is: `COBYLA_ITERATE` if
+   user is requested to compute F(X) and C(X); `COBYLA_SUCCESS` if algorithm
+   has converged; anything else indicate an error (see `cobyla_reason` for an
+   explanatory message). */
+extern int
+cobyla_get_status(const cobyla_context_t* ctx);
+
+/* Get the current number of function evaluations.  Result is -1 if something
+   is wrong (e.g. CTX is NULL), nonnegative otherwise. */
+extern opk_index_t
+cobyla_get_nevals(const cobyla_context_t* ctx);
+
+/* Get the current size of the trust region.  Result is 0 if algorithm not yet
+   started (before first iteration), -1 if something is wrong (e.g. CTX is
+   NULL), strictly positive otherwise. */
+extern double
+cobyla_get_rho(const cobyla_context_t* ctx);
+
+/* Get the last function value.  Upon convergence of `cobyla_iterate`
+   (i.e. return with status `COBYLA_SUCCESS`), this value corresponds to the
+   function at the solution; otherwise, this value corresponds to the previous
+   set of variables. */
+extern double
+cobyla_get_last_f(const cobyla_context_t* ctx);
+
+/* Get a textual explanation of the status returned by COBYLA. */
+extern const char*
+cobyla_reason(int status);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _COBYLA_H */
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * c-basic-offset: 2
+ * indent-tabs-mode: nil
+ * fill-column: 79
+ * coding: utf-8
+ * End:
+ */
---
+++ src/newuoa.h
@@ -0,0 +1,231 @@
+/*
+ * newuoa.h -
+ *
+ * Definitions for Mike Powell's NEWUOA algorithm for minimizing a function
+ * of many variables.  The method is "derivatives free" (only the function
+ * values are needed).  The algorithm is described in:
+ *
+ *   M.J.D. Powell, "The NEWUOA software for unconstrained minimization
+ *   without derivatives", in Large-Scale Nonlinear Optimization, editors
+ *   G. Di Pillo and M. Roma, Springer (2006), pages 255-297.
+ *
+ * The present code is based on the original FORTRAN version written by Mike
+ * Powell who kindly provides his code on demand (at mjdp@cam.ac.uk) and has
+ * been converted to C by É. Thiébaut.
+ *
+ * Copyright (c) 2004, Mike Powell (FORTRAN version).
+ * Copyright (c) 2015, Éric Thiébaut (C version).
+ *
+ * Read the accompanying `LICENSE` file for details.
+ */
+
+#ifndef _NEWUOA_H
+#define  _NEWUOA_H 1
+
+#include "optimpack.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Prototype of the objective function assumed by the NEWUOA routine.  The
+   returned value is the function value at X, the current variables, N is the
+   number of variables and DATA is anything else needed by the objective
+   function (unused by NEWUOA itself). */
+typedef double newuoa_objfun(const opk_index_t n, const double *x, void* data);
+
+/* This subroutine seeks the least value of a function of many variables, by
+   a trust region method that forms quadratic models by interpolation.  There
+   can be some freedom in the interpolation conditions, which is taken up by
+   minimizing the Frobenius norm of the change to the second derivative of
+   the quadratic model, beginning with a zero matrix. The arguments of the
+   subroutine are as follows.
+
+   N must be set to the number of variables and must be at least two.  NPT is
+   the number of interpolation conditions. Its value must be in the interval
+   [N+2,(N+1)(N+2)/2].
+
+   Function OBJFUN(N,X,DATA) must be provided by the user to compute the value
+   of the objective function for the variables X(1),X(2),...,X(N).  DATA is
+   anything else needed by the objective function.
+
+   Initial values of the variables must be set in X(1),X(2),...,X(N).  They
+   will be changed to the values that give the least calculated F.
+
+   RHOBEG and RHOEND must be set to the initial and final values of a trust
+   region radius, so both must be positive with RHOEND<=RHOBEG.  Typically
+   RHOBEG should be about one tenth of the greatest expected change to a
+   variable, and RHOEND should indicate the accuracy that is required in the
+   final values of the variables.
+
+   The value of IPRINT should be set to 0, 1, 2 or 3, which controls the amount
+   of printing. Specifically, there is no output if IPRINT=0 and there is
+   output only at the return if IPRINT=1. Otherwise, each new value of RHO is
+   printed, with the best vector of variables so far and the corresponding
+   value of the objective function. Further, each new value of F with its
+   variables are output if IPRINT=3.
+
+   MAXFUN must be set to an upper bound on the number of calls of OBJFUN.
+
+   The array W will be used for working space. Its length must be at least
+   (NPT+13)*(NPT+N)+3*N*(N+3)/2.
+
+   The returned value should be NEWUOA_SUCCESS, but a different value can be
+   returned upon error (see `newuoa_reason` for an explanatory message). */
+extern int newuoa(const opk_index_t n, const opk_index_t npt,
+                  newuoa_objfun* objfun, void* data,
+                  double* x, const double rhobeg, const double rhoend,
+                  const opk_index_t iprint, const opk_index_t maxfun,
+                  double* w);
+
+
+/* Possible values returned by NEWUOA. */
+#define NEWUOA_INITIAL_ITERATE       (2) /* only used internaly */
+#define NEWUOA_ITERATE               (1) /* caller is requested to evaluate
+                                            the objective function and call
+                                            newuoa_iterate */
+#define NEWUOA_SUCCESS               (0) /* algorithm converged */
+#define NEWUOA_BAD_NPT              (-1) /* NPT is not in the required
+                                            interval */
+#define NEWUOA_ROUNDING_ERRORS      (-2) /* too much cancellation in a
+                                            denominator */
+#define NEWUOA_TOO_MANY_EVALUATIONS (-3) /* maximum number of function
+                                            evaluations exceeded */
+#define NEWUOA_STEP_FAILED          (-4) /* trust region step has failed to
+                                            reduce quadratic approximation */
+#define NEWUOA_BAD_ADDRESS          (-5) /* illegal NULL address */
+#define NEWUOA_CORRUPTED            (-6) /* corrupted or misused workspace */
+
+/* Get a textual explanation of the status returned by NEWUOA. */
+extern const char* newuoa_reason(int status);
+
+/*---------------------------------------------------------------------------*/
+/* REVERSE COMMUNICATION VERSION */
+
+/* Opaque structure used by the reverse communication version of NEWUOA. */
+typedef struct _newuoa_context newuoa_context_t;
+
+/* Allocate a new reverse communication workspace for NEWUOA algorithm.  The
+   returned address is `NULL` to indicate an error: either invalid parameters
+   (external variable `errno` set to `EINVAL`), or insufficient memory
+   (external variable `errno` set to `ENOMEM`).  The arguments correspond to
+   those of `newuoa` (except that the variables X are omiited because they need
+   not be specified until the first iteration with `newuoa_iterate` and that
+   the workspace W is automatically allocated).
+
+   When no longer needed, the workspace must be deleted with `newuoa_delete`.
+
+   A typical usage is:
+   ```
+   double x[N], f;
+   newuoa_context_t* ctx;
+   x[...] = ...; // initial solution
+   ctx = newuoa_create(N, NPT, RHOBEG, RHOEND, IPRINT, MAXFUN);
+   status = newuoa_get_status(ctx);
+   while (status == NEWUOA_ITERATE) {
+     f = ...; // compute function value at X
+     status = newuoa_iterate(ctx, f, x);
+   }
+   newuoa_delete(ctx);
+   if (status != NEWUOA_SUCCESS) {
+     fprintf(stderr, "Something wrong occured in NEWUOA: %s\n",
+             newuoa_reason(status));
+   }
+   ```
+ */
+extern newuoa_context_t*
+newuoa_create(const opk_index_t n, const opk_index_t npt,
+              const double rhobeg, const double rhoend,
+              const opk_index_t iprint, const opk_index_t maxfun);
+
+/* Release ressource allocated for NEWUOA reverse communication workspace.
+   Argument can be `NULL`. */
+extern void newuoa_delete(newuoa_context_t* ctx);
+
+/* Perform the next iteration of the reverse communication version of the
+   NEWUOA algorithm.  On entry, the wokspace status must be `NEWUOA_ITERATE`,
+   `f` is the function value at `x`.  On exit, the returned value (the new
+   wokspace status) is: `NEWUOA_ITERATE` if a new trial point has been stored
+   in `x` and if user is requested to compute the function value for the new
+   point; `NEWUOA_SUCCESS` if algorithm has converged; anything else indicate
+   an error (see `newuoa_reason` for an explanatory message). */
+extern int newuoa_iterate(newuoa_context_t* ctx, double f, double* x);
+
+/* Restart NEWUOA algorithm using the same parameters.  The return value is the
+   new status of the algorithm, see `newuoa_get_status` for details. */
+extern int newuoa_restart(newuoa_context_t* ctx);
+
+/* Get the current status of the algorithm.  Result is: `NEWUOA_ITERATE` if
+   user is requested to compute F(X); `NEWUOA_SUCCESS` if algorithm has
+   converged; anything else indicate an error (see `newuoa_reason` for an
+   explanatory message). */
+extern int newuoa_get_status(const newuoa_context_t* ctx);
+
+/* Get the current number of function evaluations.  Result is -1 if something
+   is wrong (e.g. CTX is NULL), nonnegative otherwise. */
+extern opk_index_t newuoa_get_nevals(const newuoa_context_t* ctx);
+
+/* Get the current size of the trust region.  Result is 0 if algorithm not yet
+   started (before first iteration), -1 if something is wrong (e.g. CTX is
+   NULL), strictly positive otherwise. */
+extern double newuoa_get_rho(const newuoa_context_t* ctx);
+
+/*---------------------------------------------------------------------------*/
+/* FORTRAN WRAPPER */
+
+/* Remark: Depending on your FORTRAN compiler, you may have to change the
+           names of the compiled functions (it is assumed below that the
+           link name is the name of the FORTRAN subroutine converted to
+           lower case letters and with an underscore appended). */
+
+/* This subroutine is a version of NEWUOA that is callable from FORTRAN code.
+   The main difference with the C version is that the objective function
+   must be provided by the following external subroutine.
+
+   SUBROUTINE CALFUN (N,X,F) has to be provided by the user.  It must set F
+   to the value of the objective function for the current values of the
+   variables X(1),X(2),...,X(N), which are generated automatically
+   by NEWUOA. */
+extern int newuoa_(const opk_index_t* n, const opk_index_t* npt, double* x,
+                   const double* rhobeg, const double* rhoend,
+                   const opk_index_t* iprint, const opk_index_t* maxfun,
+                   double* w);
+
+/* Wrapper function to emulate `newuoa_calfun` function calling
+   the user-defined `calfun_` subroutine. */
+extern double newuoa_calfun_wrapper(const opk_index_t n, const double* x, void* data);
+
+/* Subroutine that must be defined by the application to use the FORTRAN
+   wrapper to NEWUOA. */
+extern int calfun_(const opk_index_t* n, double *x, double *f);
+
+/* Test problem for NEWUOA,FIXME: the objective function being the sum of the
+   reciprocals of all pairwise distances between the points P_I,
+   I=1,2,...,M in two dimensions, where M=N/2 and where the components of
+   P_I are X(2*I-1) and X(2*I). Thus each vector X of N variables defines
+   the M points P_I. The initial X gives equally spaced points on a
+   circle. Four different choices of the pairs (N,NPT) are tried, namely
+   (10,16), (10,21), (20,26) and (20,41). Convergence to a local minimum
+   that is not global occurs in both the N=10 cases. The details of the
+   results are highly sensitive to computer rounding errors. The choice
+   IPRINT=2 provides the current X and optimal F so far whenever RHO is
+   reduced. The bound constraints of the problem require every component of
+   X to be in the interval [-1,1]. */
+extern void newuoa_test(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _NEWUOA_H */
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * c-basic-offset: 2
+ * indent-tabs-mode: nil
+ * fill-column: 79
+ * coding: utf-8
+ * End:
+ */
