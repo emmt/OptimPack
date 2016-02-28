c1bd1fe32cf5738a0d0c7be06bdcdca35cd1f82e
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2016-02-28 08:49:07 +0100

    Fix Makefile settings

--- .dir-locals.el
+++ .dir-locals.el
@@ -7,6 +7,7 @@
 	    (c-basic-offset . 2)))
  (java-mode . ((c-basic-offset . 4)))
  (julia-mode . ())
+ (makefile-gmake-mode . ((indent-tabs-mode . t)))
  (sh-mode . ((sh-basic-offset . 4)))
  (tcl-mode . ((tcl-default-application . "wish")
 	      (tcl-indent-level . 2)))
