7a1b271d12c2062fbc1ee131ed0b592f270ae343
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2016-07-17 09:46:39 +0200

    Remove coding settings

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ((nil . ((indent-tabs-mode . nil)
 	 (tab-width . 8)
-	 (coding . utf8)
+	 ;;(coding . utf8)
 	 (fill-column . 79)
 	 (ispell-local-dictionary . "american")))
  (c-mode . ((c-file-style . "bsd")
